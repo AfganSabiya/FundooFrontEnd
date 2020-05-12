9ac687c5cfc29653d1b66d718e73d8760a0d49f1
AfganSabiya <afgansabiya786@gmail.com>
2020-05-13 02:42:02 +0530

    [Afgansabiya]Refactor. Modify register component ts file

--- FundooFrontEnd/src/app/Component/register/register.component.ts
+++ FundooFrontEnd/src/app/Component/register/register.component.ts
@@ -10,47 +10,41 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  firstName = new FormControl('', [
-    Validators.required, Validators.minLength(4), Validators.pattern('^[a-zA-Z]*'),
-  ]);
-  lastName = new FormControl('', [
-    Validators.required, Validators.minLength(4), Validators.pattern('^[a-zA-Z]*'),
-  ]);
+  constructor(
+    private service: AccountService,
+    private route: Router,
+    private snackbar: MatSnackBar) { }
+    firstName = new FormControl('', [
+    Validators.required, Validators.minLength(4), Validators.pattern('^[a-zA-Z]*'),]);
+    lastName = new FormControl('', [
+    Validators.required, Validators.minLength(4), Validators.pattern('^[a-zA-Z]*'),]);
   email = new FormControl('', [
-    Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z]{2,4}$'),
-  ]);
+    Validators.required, Validators.email,]);
   password = new FormControl('', [
-    Validators.required, Validators.minLength(8),
-  ]);
-  constructor(private service: AccountService, private route: Router, private snackbar: MatSnackBar) { }
-
+    Validators.required, Validators.minLength(8),]);
+  confirmpassword = new FormControl('', [
+    Validators.required, Validators.minLength(8),]);
   ngOnInit() {
   }
 
-  registrationform() {
-    debugger;
-     let firstname = new String(this.firstName.value);
-     let lastname = new String(this.lastName.value);
-    let pwd = new String(this.password.value);
-    if (firstname.length >= 4 && lastname.length >= 4 && pwd.length >= 8) 
-    {
-       const form = 
-       {
-         firstName: this.firstName.value,
-         lastName: this.lastName.value,
-         email: this.email.value,
-         password: this.password.value
-       };
-       this.service.registrationform(form).subscribe((result) => 
-       {
-         this.snackbar.open('Registered Sucessfully', 'Dismiss', { duration: 3000 });
-         console.log('result :', result);
-         this.route.navigate(['/login']);
-       },
-         (error) =>
-          {
-           this.snackbar.open('Registration Failed.Check Your Credentails', '', { duration: 4000 });
-          });
-    }
- }
+  registrationForm() {
+    let firstName = new String(this.firstName.value);
+    let lastName = new String(this.lastName.value);
+    let userPassword = new String(this.password.value);
+    if (firstName.length >= 4 && lastName.length >= 4 && userPassword.length >= 8) {
+      const form = {
+        firstName: this.firstName.value,
+        lastName: this.lastName.value,
+        userEmail: this.email.value,
+        userPassword: this.password.value
+      };
+      this.service.registrationForm(form).subscribe(Response => {
+        this.snackbar.open('Registered Sucessfully', 'Dismiss', { duration: 3000 });
+        this.route.navigate(['/login']);
+      },
+        (error) => {
+          this.snackbar.open('Registration Failed.Check Your Credentails', '', { duration: 4000 });
+        });
+      }
+  }
 }
