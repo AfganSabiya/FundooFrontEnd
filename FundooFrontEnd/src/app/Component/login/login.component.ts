162b9b623f0e8e731363fe6b93bc4499423a3b7a
AfganSabiya <afgansabiya786@gmail.com>
2020-05-12 17:45:37 +0530

    [Afgansabiya]Refactor. Modify login component ts

--- FundooFrontEnd/src/app/Component/login/login.component.ts
+++ FundooFrontEnd/src/app/Component/login/login.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  email = new FormControl('', [
+    Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z]{2,4}$'),
+  ]);
 
-  constructor() { }
+  constructor(
+    private service: AccountService, 
+    private route: Router, 
+    private snackbar: MatSnackBar) 
+    { }
+
 
   ngOnInit() {
   }
