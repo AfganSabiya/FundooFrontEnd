e2899c4c2242d246c21dddbea4ee656604b75293
AfganSabiya <afgansabiya786@gmail.com>
2020-05-12 17:51:27 +0530

    [Afgansabiya]Refactor. Modify login component ts

--- FundooFrontEnd/src/app/Component/login/login.component.ts
+++ FundooFrontEnd/src/app/Component/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountService } from 'src/app/Services/account.service';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
