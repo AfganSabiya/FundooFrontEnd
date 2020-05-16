d98ae37f5ee448753af5d728db5e1b22f37bbb3a
AfganSabiya <afgansabiya786@gmail.com>
2020-05-17 02:40:44 +0530

    [Afgansabiya]Add. notemodel ts file

---
+++ src/app/model/notesmode.model.ts
@@ -0,0 +1,14 @@
+export class Note {
+    title: String;
+    description: String;
+    changeColor: String;
+    pin: number;
+    trash: number;
+    archive: number;
+    remainder: string;
+    search: number;
+    addImage: string;
+    id: number;
+    email: string;
+}
+
