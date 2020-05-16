08963387630e7f0f2dc28baf7c0bff94007b94e0
AfganSabiya <afgansabiya786@gmail.com>
2020-05-17 02:39:38 +0530

    [Afgansabiya]Refactor. modify app routing module ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,23 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './Component/login/login.component';
 import { ForgotpasswordComponent } from './Component/forgotpassword/forgotpassword.component';
 import { ResetpasswordComponent } from './Component/resetpassword/resetpassword.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './Component/dashboard/dashboard.component';
 import { AddNoteComponent } from './Component/add-note/add-note.component';
 import { TestComponent } from './Component/test/test.component';
-
+import { CreatenoteComponent } from './Component/createnote/createnote.component';
+import { GetnoteComponent } from './Component/getnote/getnote.component';
+import { NoteComponent } from './Component/note/note.component';
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'forgotpassword', component: ForgotpasswordComponent },
   { path: 'resetpassword', component: ResetpasswordComponent },
-  { path: 'dashboard', component: DashboardComponent },
   { path: 'test',component:TestComponent},
-  { path: 'addnote',component:AddNoteComponent}
+  { path: 'addnote',component:AddNoteComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'createnote',component:CreatenoteComponent},
+  { path: 'getnote',component:GetnoteComponent},
+  { path: 'note',component:NoteComponent}
 ];
 
 @NgModule({
