f531a0b34460806f2e95e67335ac793bf2d9ee24
AfganSabiya <afgansabiya786@gmail.com>
2020-05-18 02:06:55 +0530

    [Afgansabiya]Refactor. modify note services ts file

--- src/app/Services/note.service.ts
+++ src/app/Services/note.service.ts
@@ -4,11 +4,18 @@ import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class NoteService {
-  constructor
-  (
-    private http: HttpClient){ }
-    addNote(values){
-      return this.http.post(environment.Url+'api/AddNotes',values);
+export class NoteService 
+{
+  header = {
+    headers: new HttpHeaders()
+       .set('Authorization',  `Bearer ${localStorage.token}`)
+   };
+  constructor( private http: HttpClient){ }
+    addNote(params)
+    {
+      return this.http.post(environment.Url+'api/AddNotes',params,this.header);
+    }
+    getnote(){
+      return this.http.get(environment.Url+'api/GetAllNotes',this.header);
     }
 }
