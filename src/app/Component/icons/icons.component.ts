714d80a5654a9a373ff18464e379440b9ac445b5
AfganSabiya <afgansabiya786@gmail.com>
2020-05-22 00:29:30 +0530

    [Afgansabiya]Refactor. modify icon component ts file

--- src/app/Component/icons/icons.component.ts
+++ src/app/Component/icons/icons.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Output,EventEmitter, Input } from '@angular/core';
+import { NoteService } from 'src/app/Services/note.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Note } from 'src/app/model/notesmode.model';
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
   styleUrls: ['./icons.component.scss']
 })
 export class IconsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  @Input() notes:any;
+@Input() note:Note=new Note();
+  constructor(private noteService:NoteService,private snackbar:MatSnackBar) { }
+  ngOnInit()
+   {}
+   isArchive()
+  {
+  debugger;
+  console.log(this.notes.id);
+  this.noteService.archive(this.notes.id).subscribe((result) => {
+  this.snackbar.open('Archive Sucessfully', 'Dismiss', { duration: 3000 });
+  });
+   
   }
-
 }
