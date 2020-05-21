3ce32315e415b9cbb9d4a2beb7401f81249a0453
AfganSabiya <afgansabiya786@gmail.com>
2020-05-22 00:30:55 +0530

    [Afgansabiya]Refactor. modify note component ts file

--- src/app/Component/note/note.component.ts
+++ src/app/Component/note/note.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input} from '@angular/core';
+import { NoteService } from 'src/app/Services/note.service';
+import { Note } from 'src/app/model/notesmode.model';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-note',
@@ -6,11 +10,23 @@ import { Component, OnInit, Input} from '@angular/core';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
-  
-  @Input() note:any;
-  constructor(
+   noteModel: Note = new Note();
+   displayAllNotes: any;
+  constructor
+  (
+    private noteservice: NoteService,
+    private snackbar: MatSnackBar
   ) { }
   ngOnInit()
-  { }
-  
+  { 
+    this.getAllNotes();
+  }
+  getAllNotes(event?) 
+  {
+    this.noteservice.getNote().subscribe(Response => 
+    {
+      this.displayAllNotes = Response;
+      console.log(this.displayAllNotes);
+    });
+  }
 }
