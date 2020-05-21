98ac8d332f4b9ba929fd5f001a1935c622728daa
AfganSabiya <afgansabiya786@gmail.com>
2020-05-22 00:32:34 +0530

    [Afgansabiya]Refactor. modify approuting ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,26 +6,26 @@ import { LoginComponent } from './Component/login/login.component';
 import { ForgotpasswordComponent } from './Component/forgotpassword/forgotpassword.component';
 import { ResetpasswordComponent } from './Component/resetpassword/resetpassword.component';
 import { DashboardComponent } from './Component/dashboard/dashboard.component';
-import { AddNoteComponent } from './Component/add-note/add-note.component';
 import { TestComponent } from './Component/test/test.component';
-import { CreatenoteComponent } from './Component/createnote/createnote.component';
+import { IconsComponent } from './Component/icons/icons.component';
 import { NoteComponent } from './Component/note/note.component';
-import { DisplayNoteComponent } from './Component/display-note/display-note.component';
+import { AddnoteComponent } from './Component/addnote/addnote.component';
+
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'forgotpassword', component: ForgotpasswordComponent },
   { path: 'resetpassword', component: ResetpasswordComponent },
   { path: 'test',component:TestComponent},
-  { path: 'addnote',component:AddNoteComponent},
+  { path: 'icon',component:IconsComponent},
+  { path: 'addnote',component: AddnoteComponent},
   { path: 'dashboard', component: DashboardComponent,
+
   children:[
-    {path:'display',component:DisplayNoteComponent},
+    {path:'note',component:NoteComponent,
+    }
   ]
 },
-  { path: 'createnote',component:CreatenoteComponent},
-  { path: 'note',component:NoteComponent},
-   {path:'displaynote',component:DisplayNoteComponent}
 ];
 
 
