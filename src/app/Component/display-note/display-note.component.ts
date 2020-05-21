e9104568bb1c47204b58a62bdd44e6910b1cbbb7
AfganSabiya <afgansabiya786@gmail.com>
2020-05-22 00:27:28 +0530

    [Afgansabiya]Refactor. modify displaynote ts file

--- src/app/Component/display-note/display-note.component.ts
+++ src/app/Component/display-note/display-note.component.ts
@@ -1,33 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { NoteService } from 'src/app/Services/note.service';
+import { Component, OnInit,Input,EventEmitter,Output} from '@angular/core';
 import { Note } from 'src/app/model/notesmode.model';
-import { ActivatedRoute } from '@angular/router';
-import { MatSnackBar } from '@angular/material';
 @Component({
   selector: 'app-display-note',
   templateUrl: './display-note.component.html',
   styleUrls: ['./display-note.component.scss']
 })
 export class DisplayNoteComponent implements OnInit {
-  param: any;
-  noteModel: Note = new Note();
-  allNotes: any;
-  constructor(
-    private noteservice: NoteService,
-    private activatedroute: ActivatedRoute,
-    private snackbar: MatSnackBar
-  ) { }
-
-  ngOnInit() {
-    this.getAllNotes();
-  }
-  getAllNotes() 
-  {
-    this.noteservice.getnote().subscribe(Response => 
-    {
-      console.log(Response);
-      this.allNotes = Response;
-    });
-  }
+  @Input() inputdata: any;
+  @Input() note:any;
+  @Input() notesmode:Note=new Note();
+  constructor() { }
+  ngOnInit() 
+  { }
 
 }
