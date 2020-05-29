031e50ffab8cbb1cee71fdf301d8dadbf392a2ff
AfganSabiya <afgansabiya786@gmail.com>
2020-05-29 20:07:20 +0530

    [Afgan]Refactor. modify LoginApplication.cs

---
+++ FundooSelenium/FundooSelenium/Page/LoginToApplication.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundooSelenium.Page
+{
+    class LoginToApplication
+    {
+        IWebDriver webdriver;
+        public LoginToApplication(IWebDriver presentdriver)
+        {
+            PageFactory.InitElements(presentdriver, this);
+            this.webdriver = presentdriver;
+        }
+     
+        [FindsBy(How = How.Id, Using = "mat-input-0")]
+        private IWebElement email;
+        [FindsBy(How = How.Id, Using = "mat-input-1")]
+        private IWebElement password;
+        [FindsBy(How = How.ClassName, Using = "mat-button-wrapper")]
+        private IWebElement loginbutton;
+
+        public void Login(string Email,string Password)
+        {
+            System.Threading.Thread.Sleep(2000);
+            email.SendKeys(Email);
+            password.SendKeys(Password);
+        }
+
+        public void ClickToLoginButton()
+        {
+            loginbutton.Submit();
+        }
+
+    }
+}
