ac76a669476e463141d9904d9bf5fb4d675c5483
AfganSabiya <afgansabiya786@gmail.com>
2020-05-29 19:31:36 +0530

    [Afgan]Refactor. modify homepage.cs file

---
+++ BookSwagon/BookSwagon/Pages/HomePage.cs
@@ -0,0 +1,22 @@
+﻿//---------------------------------------------------------------------------------------------------------------------------------------
+//<copyright file = "HomePage.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name=Afgan Sabiya"/>
+//---------------------------------------------------------------------------------------------------------------------------------------
+namespace BookSwagon.Pages
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.PageObjects;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    class HomePage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using = "account")]
+        public IWebElement MyAccount { get; set; }
+
+    }
+}
