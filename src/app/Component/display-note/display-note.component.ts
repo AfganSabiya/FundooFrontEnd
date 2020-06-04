a02d3495b509ab8e3f86d3a20eb8e0a8f59ba687
AfganSabiya <afgansabiya786@gmail.com>
2020-06-05 01:18:04 +0530

    [Afgan]Refactor. createnote html file

--- src/app/Component/display-note/display-note.component.ts
+++ src/app/Component/display-note/display-note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input,EventEmitter,Output} from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Note } from 'src/app/model/notesmode.model';
 import { NoteService } from 'src/app/Services/note.service';
 import { MatSnackBar } from '@angular/material';
@@ -10,45 +10,92 @@ import { MatSnackBar } from '@angular/material';
 export class DisplayNoteComponent implements OnInit {
   [x: string]: any;
   @Input() inputdata: any;
-  @Input() note:any;
+  @Input() note: any;
   @Input() notes: Note = new Note();
- 
+  @Output() notify: EventEmitter<any> = new EventEmitter<any>();
+  time = "8:00 AM";
+  repeat = "daily";
+  day = "Today";
   constructor(
     private noteservice: NoteService,
     private snackbar: MatSnackBar
   ) { }
-  ngOnInit() 
-  { }
-  
-  notify(event,id) {
-  debugger;
-  switch(event['name']){
-    case 'archive' :
-    this.archive(id);
-    break;
-    case 'color' :
-      this.setColor(id,event.value);
-    break;
+  ngOnInit() { }
+
+  iconEvent(event, id) {
+    debugger;
+    switch (event['name']) {
+      case 'archive':
+        this.archive(id);
+        break;
+      case 'color':
+        this.setColor(id, event.value);
+        break;
+      case 'trash':
+         this.UpdateTrash(id);
+        console.log("goingtrash");
+        break;
+      case 'deleteforever':
+        this.deleteForever(id);
+        console.log("deleting forever");
+        break;
+      case 'Restore':
+        this.restore(id);
+        console.log("restoring");
+        break;
+    }
   }
-}
-private archive(id: any){
-  debugger;
-  console.log(this.notes.id);
-  this.noteservice.archive(id).subscribe((result) => {
-  this.snackbar.open('Archive Sucessfully', 'Dismiss', { duration: 3000 });
-  });
-}
-private  setColor(id: any, event:any)
-{
-  this.noteservice.setColor(id, event).subscribe(Response=>
-  {
-    console.log(Response);
-    this.snackbar.open('Colored Sucessfully','dismiss', { duration: 4000 });
-  },
-  (error)=>{
-    console.log('error :', error );
-      this.snackbar.open('colors Failed', '', { duration: 4000 });
-  })
-}
+  private archive(id: any) {
+    console.log(this.notes.id);
+    this.noteservice.archive(id).subscribe((result) => {
+      this.snackbar.open('Archive Sucessfully', 'Dismiss', { duration: 3000 });
+    });
+  }
+
+  private setColor(id: any, event: any) {
+    this.noteservice.setColor(id, event).subscribe(Response => {
+      console.log(Response);
+      this.snackbar.open('Colored Sucessfully', 'dismiss', { duration: 4000 });
+    },
+      (error) => {
+        console.log('error :', error);
+        this.snackbar.open('colors Failed', '', { duration: 4000 });
+    });
+  }
+  
+  private restore(id: any) {
+    this.noteservice.restoreFromTrash(id).subscribe(Response => {
+      console.log(Response);
+      this.snackbar.open('Restore delete note','dismiss',{ duration: 4000 });
+    },
+    (error) => {
+      console.log('error :', error);
+      this.snackbar.open('Restoring Note failed', '', { duration: 4000 });
+    });
+
+  }
+
+  private deleteForever(id) {
+    this.noteservice.deleteNote(id).subscribe(Response => 
+    {
+      console.log(Response);
+      this.snackbar.open('Note deleted forever','dismiss',{ duration: 4000 });
+    },
+    (error) => {
+      console.log('error :', error);
+      this.snackBar.open('error in deleting note', '', { duration: 4000 });
+    });
+  }
+
+  private UpdateTrash(id) {
+    this.noteservice.sendToTrash(id).subscribe((result) => {
+      console.log('response', result);
+      this.snackBar.open('Note sent to trash', 'Dismiss', { duration: 4000 });
+    },
+      (error) => {
+        console.log('error :', error);
+        this.snackBar.open('error in sending to trash', '', { duration: 4000 });
+      });
+  }
 
 }
