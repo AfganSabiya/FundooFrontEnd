0e65d9b929f8d2aba021ed66165f13a6ee6ba288
AfganSabiya <afgansabiya786@gmail.com>
2020-06-05 01:20:14 +0530

    [Afgan]Refactor. modify displaynote.component ts file

--- src/app/Component/display-note/display-note.component.ts
+++ src/app/Component/display-note/display-note.component.ts
@@ -8,31 +8,31 @@ import { MatSnackBar } from '@angular/material';
   styleUrls: ['./display-note.component.scss']
 })
 export class DisplayNoteComponent implements OnInit {
-  [x: string]: any;
-  @Input() inputdata: any;
-  @Input() note: any;
+  @Input() inputnote: any;
+  @Input() trash:boolean=false;
   @Input() notes: Note = new Note();
   @Output() notify: EventEmitter<any> = new EventEmitter<any>();
-  time = "8:00 AM";
-  repeat = "daily";
-  day = "Today";
+  
   constructor(
     private noteservice: NoteService,
     private snackbar: MatSnackBar
   ) { }
   ngOnInit() { }
-
+  
   iconEvent(event, id) {
     debugger;
     switch (event['name']) {
       case 'archive':
         this.archive(id);
         break;
+      case 'unarchive':
+        this.unArchive(id);
+        break;
       case 'color':
         this.setColor(id, event.value);
         break;
       case 'trash':
-         this.UpdateTrash(id);
+         this.DeleteNotegotoTrash(id);
         console.log("goingtrash");
         break;
       case 'deleteforever':
@@ -43,18 +43,28 @@ export class DisplayNoteComponent implements OnInit {
         this.restore(id);
         console.log("restoring");
         break;
+      default:
+        this.notify.emit({ id: id, name: event.name });
+        break;
     }
   }
-  private archive(id: any) {
-    console.log(this.notes.id);
+  private archive(id) {
+    //console.log(this.notes.id);
+    this.notify.emit({name:'getAllNotes'})
     this.noteservice.archive(id).subscribe((result) => {
       this.snackbar.open('Archive Sucessfully', 'Dismiss', { duration: 3000 });
     });
   }
-
+  private unArchive(id: any){
+    console.log(this.notes.id);
+    this.noteservice.unarchive(id).subscribe((result) => {
+         this.snackbar.open('Unarchive Sucessfully','dismiss', { duration: 4000 });
+     })
+  }
   private setColor(id: any, event: any) {
     this.noteservice.setColor(id, event).subscribe(Response => {
       console.log(Response);
+      this.notify.emit({name:'getAllNotes'})
       this.snackbar.open('Colored Sucessfully', 'dismiss', { duration: 4000 });
     },
       (error) => {
@@ -62,9 +72,21 @@ export class DisplayNoteComponent implements OnInit {
         this.snackbar.open('colors Failed', '', { duration: 4000 });
     });
   }
-  
+  private DeleteNotegotoTrash(id) {
+    this.noteservice.sendToTrash(id).subscribe((Response) => {
+      this.notify.emit({name:'getAllNotes'})
+       this.snackbar.open('Note sent to trash', 'Dismiss', { duration: 4000 });
+      console.log(Response);
+    
+    },
+      (error) => {
+        console.log('error :', error);
+        this.snackbar.open('error in sending to trash', '', { duration: 4000 });
+      });
+  }
   private restore(id: any) {
     this.noteservice.restoreFromTrash(id).subscribe(Response => {
+      this.notify.emit({name:'getAllNotes'})
       console.log(Response);
       this.snackbar.open('Restore delete note','dismiss',{ duration: 4000 });
     },
@@ -74,28 +96,16 @@ export class DisplayNoteComponent implements OnInit {
     });
 
   }
-
   private deleteForever(id) {
     this.noteservice.deleteNote(id).subscribe(Response => 
     {
+      this.notify.emit({name:'getAllNotes'})
       console.log(Response);
       this.snackbar.open('Note deleted forever','dismiss',{ duration: 4000 });
     },
     (error) => {
       console.log('error :', error);
-      this.snackBar.open('error in deleting note', '', { duration: 4000 });
+      this.snackbar.open('error in deleting note', '', { duration: 4000 });
     });
   }
-
-  private UpdateTrash(id) {
-    this.noteservice.sendToTrash(id).subscribe((result) => {
-      console.log('response', result);
-      this.snackBar.open('Note sent to trash', 'Dismiss', { duration: 4000 });
-    },
-      (error) => {
-        console.log('error :', error);
-        this.snackBar.open('error in sending to trash', '', { duration: 4000 });
-      });
-  }
-
 }
