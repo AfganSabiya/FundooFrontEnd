f9615e534d7213ee9eba634437eb9008077fd41c
AfganSabiya <afgansabiya786@gmail.com>
2020-06-12 12:02:48 +0530

    [Afgan]Refactor. modify icons component ts file

--- src/app/Component/icons/icons.component.ts
+++ src/app/Component/icons/icons.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit,Output,EventEmitter, Input } from '@angular/core';
-import { NoteService } from 'src/app/Services/note.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Note } from 'src/app/model/notesmode.model';
+
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
@@ -10,27 +8,24 @@ import { Note } from 'src/app/model/notesmode.model';
 export class IconsComponent implements OnInit {
   [x: string]: any; 
   @Input() notes:any;
-  note:Note = new Note();
   @Input() onDisplay:boolean = false;
- @Input() onCreateNote:boolean = false;
   @Output() output:EventEmitter<any> = new EventEmitter<any>();
   @Input() trash:boolean=false;
   @Input() archive:boolean=false;
-  
- constructor()
-     { }
-  ngOnInit()
-  { }
+  todayString : string = new Date().toDateString();
+ constructor(
+  ) { }
+  ngOnInit(){ }
+
   isArchive(){
-    this.output.emit({ name: 'archive' });
+    this.output.emit({ name: 'archive',value:""});
   }
-  
-  setColor(color)
+    setColor(color)
   {
     this.output.emit({ name: 'color', value:color});
   }
   DeleteNotegotoTrash(){
-    this.output.emit({ name: 'trash'});
+    this.output.emit({ name: 'trash',value:""});
   }
    restore() {
     this.output.emit({ name: 'Restore'});
@@ -38,25 +33,33 @@ export class IconsComponent implements OnInit {
    deleteForever() {
     this.output.emit({ name: 'deleteforever'});
    }
-
-  colors = [
+   saveReminder(date) {
+    this.output.emit({ name:"remainder" ,value:date});
+  }
+   colors =[
     [
-      { color: "rgb(255,255,255)", name: "Default" },
-      { color: "rgb(242, 139, 130)", name: "Red" },
-      { color: "rgb(251, 188, 4)", name: "Orange" },
-      { color: "rgb(255, 244, 117)", name: "Yellow" }
+      { color: "rgba(255,255,255)", name: "Default" },
+      { color: "rgba(242, 139, 130)", name: "Red" },
+      { color: "rgba(251, 188, 4)", name: "Orange" },
+      { color: "rgba(255, 244, 117)", name: "Yellow" }
     ],
     [
-      { color: "rgb(204, 255, 144)", name: "Green" },
-      { color: "rgb(167, 255, 235)", name: "Teal" },
-      { color: "rgb(203, 240, 248)", name: "Blue" },
-      { color: "rgb(174, 203, 250)", name: "Dark blue"}
+      { color: "rgba(204, 255, 144)", name: "Green" },
+      { color: "rgba(167, 255, 235)", name: "Teal" },
+      { color: "rgba(203, 240, 248)", name: "Blue" },
+      { color: "rgba(174, 203, 250)", name: "Dark blue"}
     ],
     [
-      { color: "rgb(215, 174, 251)", name: "Purple" },
-      { color: "rgb(253, 207, 232)", name: "Pink" },
-      { color: "rgb(230, 201, 168)", name: "Brown" },
-      { color: "rgb(232, 234, 237)", name: "Gray" }
+      { color: "rgba(215, 174, 251)", name: "Purple" },
+      { color: "rgba(253, 207, 232)", name: "Pink" },
+      { color: "rgba(230, 201, 168)", name: "Brown" },
+      { color: "rgba(232, 234, 237)", name: "Gray" }
     ]
   ]
+  setTime(time) {
+    this.time = time;
+  }
+  setRepitation(repeat) {
+    this.repeat = repeat;
+  }
 }
