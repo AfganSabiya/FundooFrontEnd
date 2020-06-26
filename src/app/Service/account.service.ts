c73cb6b3613b588bd720789bff84aedee131e950
AfganSabiya <afgansabiya786@gmail.com>
2020-06-26 18:54:12 +0530

    [Afgan]Added. account service ts file

---
+++ src/app/Service/account.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  registrationForm(values){
+    console.log(values);
+    return this.http.post(environment.Url+'api/Register',values);
+  }
+  loginForm(data)
+  {
+    return this.http.post(environment.Url+'api/AdminLogin',data);
+  }
+}
