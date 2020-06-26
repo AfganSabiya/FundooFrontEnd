acb198d0b57ac22ae0ffa666fa2a366da16f312f
AfganSabiya <afgansabiya786@gmail.com>
2020-06-26 18:14:52 +0530

    [Afgan]Refactor. modify app module ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-//import { NgModule } from '@angular/core';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +11,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatGridListModule} from '@angular/material/grid-list';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -27,7 +25,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatDividerModule } from '@angular/material/divider';
 import {MatTooltipModule} from '@angular/material/tooltip';
-import { CreatenoteComponent } from './Component/createnote/createnote.component';
+import {CreatenoteComponent } from './Component/createnote/createnote.component';
 import { MatListModule } from '@angular/material/list';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
@@ -35,7 +33,30 @@ import { TestComponent } from './Component/test/test.component';
 import { DisplayNoteComponent } from './Component/display-note/display-note.component';
 import { IconsComponent } from './Component/icons/icons.component';
 import { NoteComponent } from './Component/note/note.component';
-import { AddnoteComponent } from './Component/addnote/addnote.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material';
+import { TrashComponent } from './Component/trash/trash.component';
+import { ArchiveComponent } from './Component/archive/archive.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { UpdateNoteComponent } from './Component/update-note/update-note.component';
+import { RemainderComponent } from './Component/remainder/remainder.component';
+import { AccountService } from './Services/account.service';
+import { NoteService } from './Services/note.service';
+import { ProfilePicComponent } from './Component/profile-pic/profile-pic.component';
+import { DataService } from './Services/data.service';
+import { TestingComponent } from './Component/testing/testing.component';
+import { SearchComponent } from './Component/search/search.component';
+import { LabelComponent } from './Component/label/label.component';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { ProfileComponent } from './profile/profile.component';
+import { ImageprofileComponent } from './Component/imageprofile/imageprofile.component';
+import { CollaboratorComponent } from './Component/collaborator/collaborator.component';
+import { CollaboratorService } from './Services/collaborator.service';
 
 @NgModule({
   declarations: [
@@ -46,25 +67,41 @@ import { AddnoteComponent } from './Component/addnote/addnote.component';
     ResetpasswordComponent,
     DashboardComponent,
     IconsComponent,
-    CreatenoteComponent,
+  CreatenoteComponent,
     TestComponent,
     DisplayNoteComponent,
     NoteComponent,
-    AddnoteComponent,
-  ],
+    TrashComponent,
+    ArchiveComponent,
+    UpdateNoteComponent,
+    RemainderComponent,
+    ProfilePicComponent,
+    TestingComponent,
+    SearchComponent,
+    LabelComponent,
+    ProfileComponent,
+    ImageprofileComponent,
+    CollaboratorComponent,
+  
+   ],
   imports: [
     FormsModule,
     BrowserModule,
+    ImageCropperModule,
+    FlexLayoutModule,
     HttpClientModule,
     MatExpansionModule,
     MatSelectModule,
     MatIconModule,
     AppRoutingModule,
+    MatDatepickerModule,
+    NgxMaterialTimepickerModule,
     MatGridListModule,
     MatToolbarModule,
     MatMenuModule,
     MatSnackBarModule, 
     RouterModule,
+    MatDialogModule,
     MatSidenavModule,
     MatTooltipModule,
     MatInputModule,
@@ -76,10 +113,22 @@ import { AddnoteComponent } from './Component/addnote/addnote.component';
     MatButtonModule,
     MatListModule,
     MatFormFieldModule,
+    MatNativeDateModule,
+    MatChipsModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  entryComponents:[UpdateNoteComponent,ProfilePicComponent,CollaboratorComponent],
+  providers: 
+  [
+    AccountService,
+    DataService,
+    NoteService,
+    CollaboratorService,
+    {provide: MatDialogRef,useValue:{}},
+    {provide: MAT_DIALOG_DATA,useValue:[]} ,
+  ],
   bootstrap: [AppComponent],
+  
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
   ]
