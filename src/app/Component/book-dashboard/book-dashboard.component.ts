43b9755f846ece1da4ce3fa31c6abdcf658ea407
AfganSabiya <afgansabiya786@gmail.com>
2020-07-08 23:30:06 +0530

    [Afgan]Refactor. modify dashboard component file

--- src/app/Component/book-dashboard/book-dashboard.component.ts
+++ src/app/Component/book-dashboard/book-dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit,ChangeDetectorRef } from '@angular/core';
 import {MediaMatcher} from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/Service/cart.service';
+import { DatasharingService } from 'src/app/Service/datasharing.service';
 
 @Component({
   selector: 'app-book-dashboard',
@@ -11,15 +14,36 @@ export class BookDashboardComponent implements OnInit {
   fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
   fillerContent = Array.from({length: 50}, () => ``);
   private _mobileQueryListener: () => void;
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+
+  cartCount:any;
+  
+  constructor
+  (
+    private route: Router, 
+    private cartservice: CartService,
+    private datasharing:DatasharingService,
+    changeDetectorRef: ChangeDetectorRef, media: MediaMatcher)
+     {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
   ngOnInit(): void {
+  this.getcartCount();
+
+    //this.datasharing.count.subscribe(countbooks=>this.cartCount=countbooks);
   }
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+  // shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+  getcartCount(){
+    this.cartservice.getCartCount().subscribe(
+      Response =>{
+        this.cartCount = Response['result'];
+       // console.log(Response);
+      })
+  }
+ 
+ 
 }
