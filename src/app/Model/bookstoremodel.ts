2522add87c327be892718bfa56de18665aca6ec2
AfganSabiya <afgansabiya786@gmail.com>
2020-07-09 11:42:23 +0530

    [Afgan]Refactor. modify bookstore model ts file

---
+++ src/app/Model/bookstoremodel.ts
@@ -0,0 +1,10 @@
+export class BookStoreModel
+{
+     bookID: number;
+     bookTitle: string;
+     authorName: string;
+     description:string; 
+     price : number;
+     bookImage :string;
+     bookCount :number;
+}
