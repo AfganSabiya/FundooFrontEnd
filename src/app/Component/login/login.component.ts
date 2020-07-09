873dd78933f775fd11ad1742663d402eac03c71f
AfganSabiya <afgansabiya786@gmail.com>
2020-07-09 11:40:31 +0530

    [Afgan]Refactor. modify login component file

--- src/app/Component/login/login.component.ts
+++ src/app/Component/login/login.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { AccountService } from 'src/app/Service/account.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormControl, Validators } from '@angular/forms';
-import { invalid } from '@angular/compiler/src/render3/view/util';
+import { MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
@@ -12,17 +12,20 @@ import { invalid } from '@angular/compiler/src/render3/view/util';
 })
 export class LoginComponent implements OnInit {
 
+user = '1'
   constructor(
     private service: AccountService, 
     private route: Router, 
     private snackBar: MatSnackBar) 
     { }
   ngOnInit(): void {
+    localStorage.setItem('SessionUser',this.user);
   }
  
   userEmail=[];
   email = new FormControl('', [
-    Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'),
+    Validators.required, Validators.email,
+    Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'),
   ]);
   password = new FormControl('', [
     Validators.required, Validators.minLength(8),
@@ -30,24 +33,24 @@ export class LoginComponent implements OnInit {
 
   loginForm()
   {
-    // let UserPassword = new String(this.password.value);
-    // if (this.email.value != null && UserPassword.length >= 8) {
-      const credentials= {
+       if (this.email.value != null && this.password.value !=null) {
+       const credentials= {
         UserEmail: this.email.value,
         UserPassword: this.password.value
       };
       this.service.loginForm(credentials).subscribe((response) => 
       {
         console.log(response);
+        if(Response !=null){
         localStorage.setItem('Email',credentials.UserEmail);
         localStorage.setItem('token',response['token']);
-        this.snackBar.open('Login Successfully', 'Dismiss', { duration: 3000 });
-        this.route.navigate(['/dashboard']);
+        this.snackBar.open('Login Successfully','Dismiss', { duration: 3000 });
+        this.route.navigate(['/dashboard']);}
       },(error) => {
-        console.log('error :', error );
+     console.log('error :', error );
         this.snackBar.open('Login Failed. Check Your Credentials', '', { duration: 4000 });
       });
     }
-  //}
+ }
 
 }
