5a6e747ddc7bf1b7583f5756da99f8ddc1ea47e6
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:55:58 +0530

    [Afgan]Refactor. modify IBookAccount Manager

---
+++ BookStoreApplication/BookStoreBussinessLayer/ManagerInterface/IBookAccountManager.cs
@@ -0,0 +1,25 @@
+﻿using BookStoreDataLayer.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreBussinessLayer.ManagerInterface
+{
+    public interface IBookAccountManager
+    {
+        /// <summary>
+        /// Method For Registration
+        /// </summary>
+        /// <param name="registrationModel">The Registration .</param>
+        /// <returns></returns>
+        Task<string> Register(RegisterModel registerModel);
+
+        /// <summary>
+        /// Method  for Login
+        /// </summary>
+        /// <param name="loginModel">The login .</param>
+        /// <returns></returns>
+        string Login(LoginModel loginModel);
+    }
+}
