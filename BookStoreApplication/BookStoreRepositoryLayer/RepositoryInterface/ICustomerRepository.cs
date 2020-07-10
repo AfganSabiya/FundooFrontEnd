edf4bc1946184e4812b3e78934fdf420f0373546
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:49:21 +0530

    [Afgan]Refactor. modify ICustomerRepository

---
+++ BookStoreApplication/BookStoreRepositoryLayer/RepositoryInterface/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookStoreDataLayer.CustomerModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreRepositoryLayer.RepositoryInterface
+{
+   public interface ICustomerRepository
+   {
+        string AddCustomerDetails(CustomerModel customerModel);
+        string DeleteDetails(int id);
+        List<CustomerModel> GetCustomerDetails();
+        string UpdateCustomer(CustomerModel customermodel);
+        
+    }
+}
