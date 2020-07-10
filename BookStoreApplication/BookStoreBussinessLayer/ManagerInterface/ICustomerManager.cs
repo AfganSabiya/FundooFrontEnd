5c94af91e0023fa479b3ec28c1f04bdc8ee59b95
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:56:53 +0530

    [Afgan]Refactor. modify ICustomer Manager

---
+++ BookStoreApplication/BookStoreBussinessLayer/ManagerInterface/ICustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using BookStoreDataLayer.CustomerModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreBussinessLayer.ManagerInterface
+{
+    public interface ICustomerManager 
+    {
+        string AddCustomerDetails(CustomerModel customerModel);
+        string DeleteDetails(int id);
+        List<CustomerModel> GetCustomerDetails();
+        
+        string UpdateCustomer(CustomerModel customermodel);
+    }
+
+}
