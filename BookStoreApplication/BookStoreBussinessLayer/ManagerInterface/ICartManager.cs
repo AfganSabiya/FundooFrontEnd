4f901dd8fa8728f662c810c9a2091bc141701228
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:56:39 +0530

    [Afgan]Refactor. modify ICart Manager

---
+++ BookStoreApplication/BookStoreBussinessLayer/ManagerInterface/ICartManager.cs
@@ -0,0 +1,45 @@
+﻿using BookStoreDataLayer.BookModel;
+using BookStoreDataLayer.CartModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreBussinessLayer.ManagerInterface
+{
+    public interface ICartManager
+    {
+        /// <summary>
+        /// Adds the cart.
+        /// </summary>
+        /// <param name="cartModel">The cart model.</param>
+        /// <returns></returns>
+        Task<string> AddCart(CartModel cartModel);
+
+        /// <summary>
+        /// Deletes the cart.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Task<string> DeleteCart(int id);
+
+        /// <summary>
+        /// Counts the cart.
+        /// </summary>
+        /// <returns></returns>
+        int CountCart();
+
+        /// <summary>
+        /// Gets all cart value.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<object> GetAllCartValue(string email);
+
+        //IQueryable GetCartValue(string email);
+     
+        int countcartbook(string email);
+
+        string UpdateBook(CartModel cartModel);
+    }
+}
