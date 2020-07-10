82e4f894fd2f8a6c1e7e55ef0d677b201b2e5d0b
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:42:57 +0530

    [Afgan]Refactor. modify Customer Model

---
+++ BookStoreApplication/BookStoreDataLayer/CustomerModel/CustomerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookStoreDataLayer.CustomerModel
+{
+    public class CustomerModel
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Pincode { get; set; }
+        public string Locality { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string LandMark { get; set; }
+        public string Type { get; set; }
+       
+    }
+}
