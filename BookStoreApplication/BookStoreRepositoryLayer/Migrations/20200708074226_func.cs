6fbc8888d194c73682d77f70c3536935860c9b58
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:46:47 +0530

    [Afgan]added. updated Migrations

---
+++ BookStoreApplication/BookStoreRepositoryLayer/Migrations/20200708074226_func.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStoreRepositoryLayer.Migrations
+{
+    public partial class func : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookStore",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BookTitle = table.Column<string>(nullable: false),
+                    AuthorName = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    BookImage = table.Column<string>(nullable: false),
+                    BookCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookStore", x => x.BookID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookStoreAccount",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    UserEmail = table.Column<string>(nullable: false),
+                    UserPassword = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookStoreAccount", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartTable",
+                columns: table => new
+                {
+                    CartID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BookID = table.Column<int>(nullable: false),
+                    SelectBookCount = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartTable", x => x.CartID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerTable",
+                columns: table => new
+                {
+                    CustomerID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    Pincode = table.Column<string>(nullable: true),
+                    Locality = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    LandMark = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    OrderId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerTable", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WishTable",
+                columns: table => new
+                {
+                    WishId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    bookID = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WishTable", x => x.WishId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookStore");
+
+            migrationBuilder.DropTable(
+                name: "BookStoreAccount");
+
+            migrationBuilder.DropTable(
+                name: "CartTable");
+
+            migrationBuilder.DropTable(
+                name: "CustomerTable");
+
+            migrationBuilder.DropTable(
+                name: "WishTable");
+        }
+    }
+}
