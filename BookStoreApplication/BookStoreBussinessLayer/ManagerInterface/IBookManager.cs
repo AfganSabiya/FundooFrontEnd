295b8014e45443710fa42d50d07e8fb214e19f59
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:56:15 +0530

    [Afgan]Refactor. modify IBook Manager

---
+++ BookStoreApplication/BookStoreBussinessLayer/ManagerInterface/IBookManager.cs
@@ -0,0 +1,19 @@
+﻿using BookStoreDataLayer.BookModel;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreBussinessLayer.ManagerInterface
+{
+    public interface IBookManager
+    {
+        List<BookStoreModel> GetALLBooks();
+        Task<string> AddBooksDetail(BookStoreModel bookStoreModel);
+        Task<string> DeleteBook(int id);
+        Task<string> UploadImage(IFormFile formFile, int id);
+        int CountBooks();
+        string UpdateBookDetails(BookStoreModel bookStoreModel);
+    }
+}
