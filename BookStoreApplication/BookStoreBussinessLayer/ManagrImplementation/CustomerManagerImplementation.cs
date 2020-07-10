c31201ae216ed6f4c4f8aa43ee15113efeb40b76
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 19:16:38 +0530

    [Afgan]Refactor. modify Customer Manager Implementation

---
+++ BookStoreApplication/BookStoreBussinessLayer/ManagrImplementation/CustomerManagerImplementation.cs
@@ -0,0 +1,71 @@
+﻿using BookStoreBussinessLayer.ManagerInterface;
+using BookStoreDataLayer.CustomerModel;
+using BookStoreRepositoryLayer.RepositoryInterface;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreBussinessLayer.ManagrImplementation
+{
+    public class CustomerManagerImplementation : ICustomerManager
+    {
+
+        private ICustomerRepository customerRepository;
+        public CustomerManagerImplementation(ICustomerRepository repository)
+        {
+            this.customerRepository = repository;
+        }
+        public string AddCustomerDetails(CustomerModel customerModel)
+        {
+            try
+            {
+                var data = this.customerRepository.AddCustomerDetails(customerModel);
+                return data;
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public string DeleteDetails(int id)
+        {
+            try
+            {
+               return this.customerRepository.DeleteDetails(id);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public List<CustomerModel> GetCustomerDetails()
+        {
+            try
+            {
+                var data = this.customerRepository.GetCustomerDetails();
+                return data;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public string UpdateCustomer(CustomerModel customermodel)
+        {
+            try
+            {
+                var data = this.customerRepository.UpdateCustomer(customermodel);
+                return data;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
