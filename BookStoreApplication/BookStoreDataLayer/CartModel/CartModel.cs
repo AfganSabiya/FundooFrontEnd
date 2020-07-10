7ffe898595673a0760f72e8494c7d9531d29899f
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:39:50 +0530

    [Afgan]Refactor. modify CartModel

---
+++ BookStoreApplication/BookStoreDataLayer/CartModel/CartModel.cs
@@ -0,0 +1,33 @@
+﻿
+namespace BookStoreDataLayer.CartModel
+{
+    using DataAnnotationsExtensions;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Text;
+
+    public class CartModel
+    {
+       
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int CartID { get; set; }
+
+    
+        [RegularExpression("^([1-9][0-9]{1,3}|100)$")]
+        [Required(ErrorMessage = "Book_ID should not be empty")]
+        public int BookID { get; set; }
+
+        
+        [RegularExpression("^([1-9][0-9]{1,3}|100)$")]
+        [Required(ErrorMessage = "SelectBookCount should not be empty")]
+        public int SelectBookCount { get; set; }
+
+        [Email]
+        [RegularExpression(@"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+    }
+}
