09db2ecb32dfacae649f8321c1df7d40f6a0195a
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:34:51 +0530

    [Afgan]Refactor. modify bookmodel

---
+++ BookStoreApplication/BookStoreDataLayer/BookModel/BookStoreModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BookStoreDataLayer.BookModel
+{
+    public class BookStoreModel
+    {
+       
+        [Key]
+        public int BookID { get; set; }
+
+        [Required(ErrorMessage = "BookTitle cannot be empty")]
+       // [RegularExpression(@"[^[a-zA-Z][a-zA-Z\\s]+$")]
+        public string BookTitle { get; set; }
+
+        [Required(ErrorMessage = "AuthorName cannot be empty")]
+       // [RegularExpression(@"[^[a-zA-Z][a-zA-Z\\s]+$")]
+        public string AuthorName { get; set; }
+
+        [Required(ErrorMessage = "Description cannot be empty")]
+      //  [RegularExpression(@"[^[a-zA-Z][a-zA-Z\\s]+$")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Price cannot be empty")]
+       // [RegularExpression("^([1-9][0-9]{1,3}|50000)$")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "BookImage cannot be empty")]
+        public string  BookImage { get; set; }
+
+        [Required(ErrorMessage = "BookCount cannot be empty")]
+        public int BookCount { get; set; }
+
+    }
+}
