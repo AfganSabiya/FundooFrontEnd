e901cf9f3d5739143a25ee067e85140ae3c031c0
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:48:23 +0530

    [Afgan]Refactor. modify IBookAccount Repository

---
+++ BookStoreApplication/BookStoreRepositoryLayer/RepositoryInterface/IBookAccountRepository.cs
@@ -0,0 +1,32 @@
+﻿using BookStoreDataLayer.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreRepositoryLayer.RepositoryInterface
+{
+   public interface IBookAccountRepository
+    {
+        /// <summary>
+        /// Method For Register Account.
+        /// </summary>
+        /// <param name="registrationModel">The register .</param>
+        /// <returns></returns>
+        Task<string> Register(RegisterModel registerModel);
+
+        /// <summary>
+        /// Method for JwtTokenGeneate
+        /// </summary>
+        /// <param name="loginModel">The login .</param>
+        /// <returns></returns>
+       // string Login(LoginModel loginModel);
+        string JWTTokenGenerate(LoginModel loginModel);
+        /// <summary>
+        /// Method For Login
+        /// </summary>
+        /// <param name="loginModel">login model .</param>
+        /// <returns></returns>
+        string Login(LoginModel loginModel);
+    }
+}
