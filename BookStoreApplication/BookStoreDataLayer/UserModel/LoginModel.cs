1698392a6af4cee255f2739c9d9fcb7d149171ab
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:40:19 +0530

    [Afgan]Refactor. modify UserModel

---
+++ BookStoreApplication/BookStoreDataLayer/UserModel/LoginModel.cs
@@ -0,0 +1,28 @@
+﻿using DataAnnotationsExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BookStoreDataLayer.UserModel
+{
+   public class LoginModel
+    {
+        /// <summary>
+        /// Generate Gets or Sets method for UserEmail
+        /// </summary>
+        [Email]
+        [Required(ErrorMessage = "Email is required")]
+       // [RegularExpression(@"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$", ErrorMessage = "Please enter a valid email address")]
+        public string UserEmail { get; set; }
+
+        /// <summary>
+        /// Generate Gets or Sets method for UserPassword
+        /// </summary>
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        //[RegularExpression(@"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$",ErrorMessage = "Please enter a valid email address")]
+        public string UserPassword { get; set; }
+   }
+}
---
+++ BookStoreApplication/BookStoreDataLayer/UserModel/RegisterModel.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace BookStoreDataLayer.UserModel
+{
+    using DataAnnotationsExtensions;
+    using Microsoft.Web.Mvc.Controls;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Text;
+    [System.Serializable]
+    public class RegisterModel 
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+
+        [RegularExpression(@"[^[a-zA-Z][a-zA-Z\\s]+$")]
+        [Required(ErrorMessage = "LastName is required")]
+        public string FirstName { get; set; }
+
+        [RegularExpression(@"[^[a-zA-Z][a-zA-Z\\s]+$")]
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; }
+
+        [Email]
+        [RegularExpression(@"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]
+        [Required(ErrorMessage = "Email is required")]
+        public string UserEmail { get; set; }
+
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]
+        [Required(ErrorMessage = "Password is required")]
+        public string UserPassword { get; set; }
+
+    }
+}
+
