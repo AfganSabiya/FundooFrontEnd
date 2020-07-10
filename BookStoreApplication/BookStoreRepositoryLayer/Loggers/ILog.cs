dfee16dbd15483006c85c4d5a5760bdf506bae26
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:46:12 +0530

    [Afgan]added. Loggers file

---
+++ BookStoreApplication/BookStoreRepositoryLayer/Loggers/ILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStoreRepositoryLayer.Loggers
+{
+    public interface ILog
+    {
+        void Information(string message);
+        void Warning(string message);
+        void Debug(string message);
+        void Error(string message);
+
+    }
+}
---
+++ BookStoreApplication/BookStoreRepositoryLayer/Loggers/LogNLog.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ILogger = NLog.ILogger;
+using LogLevel = NLog.LogLevel;
+
+namespace BookStoreRepositoryLayer.Loggers
+{
+    public class LogNLog : ILog
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public LogNLog()
+        {
+        }
+
+        public void Debug(string message)
+        {
+            Logger logger = LogManager.GetLogger("EventLogTarget");
+            var logEventInfo = new LogEventInfo(LogLevel.Error, "EventLogMessage", $"{message}, generated at {DateTime.UtcNow}.");
+            logger.Log(logEventInfo);
+            //LogManager.Shutdown();  
+        }
+
+        public void Error(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void Information(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
+
