5075d61474b26094b0d9dfc1df9139e24ebf89ac
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:48:44 +0530

    [Afgan]Refactor. modify IBookRepository

---
+++ BookStoreApplication/BookStoreRepositoryLayer/RepositoryInterface/IBookRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookStoreDataLayer.BookModel;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreRepositoryLayer.RepositoryInterface
+{
+   public  interface IBookRepository
+   {
+        List<BookStoreModel> GetALLBooks();
+        Task<string> AddBooksDetail(BookStoreModel bookStoreModel);
+        Task<string> DeleteBook(int id);
+        Task<string> UploadImage(IFormFile formFile, int id);
+        int CountBook();
+        string UpdateBookDetails(BookStoreModel bookStoreModel);
+
+
+   }
+}
