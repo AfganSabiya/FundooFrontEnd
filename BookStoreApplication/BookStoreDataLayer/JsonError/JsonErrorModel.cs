e13231131653906535935bf23b03ebfc85aca120
AfganSabiya <afgansabiya786@gmail.com>
2020-07-10 18:41:00 +0530

    [Afgan]Refactor. modify Json Error Model

---
+++ BookStoreApplication/BookStoreDataLayer/JsonError/JsonErrorModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStoreDataLayer.JsonError
+{
+   public class JsonErrorModel
+   {
+        public int ErrorCode { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
